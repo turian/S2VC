dd3a128fe2006bcd7cdeeaaf6ec4d7e0d00daf10
Joseph Turian <turian@gmail.com>
2021-09-29 13:56:34 +0200

    data3

--- data/__init__.py
+++ data/__init__.py
@@ -1,3 +1,4 @@
 from .preprocess_dataset import PreprocessDataset
 from .intra_speaker_dataset import IntraSpeakerDataset, collate_batch
+from .intra_speaker_dataset import IntraSpeakerDataset3, collate_batch3
 from .utils import *
