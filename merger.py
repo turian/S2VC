8c1d63b91c8d4389ac2c7e4eac05d98401570454
Joseph Turian <turian@gmail.com>
2021-09-29 23:43:43 +0200

    Smarter merge

--- merger.py
+++ merger.py
@@ -2,6 +2,8 @@ import os
 import sys
 import json
 
+from collections import defaultdict
+
 """Merge the metadata.json of different features"""
 
 dataset_dir = sys.argv[1]
@@ -19,11 +21,11 @@ feature_names = set()
 for key in metas[0].keys():
     if key == 'feature_name':
         continue
-    merged[key] = [{} for i in range(len(metas[0][key]))]
+    merged[key] = defaultdict(dict)
     for subdir, meta in zip(sub_dirs, metas):
-        for idx, value in enumerate(meta[key]):
-            merged[key][idx]['audio_path'] = value['audio_path']
-            merged[key][idx][meta['feature_name']] = os.path.join(subdir, value['feature_path'])
+        for value in meta[key]:
+            merged[key][value['audio_path']]['audio_path'] = value['audio_path']
+            merged[key][value['audio_path']]['feature_name'] = os.path.join(subdir, value['feature_path'])
             feature_names.add(meta['feature_name'])
 
 # Remove things that don't have both features
@@ -31,7 +33,7 @@ before = 0
 after = 0
 for key in merged:
     before += len(merged[key])
-    merged[key] = [row for row in merged[key] if feature_names <= set(row.keys())]
+    merged[key] = [row for row in merged[key].values() if feature_names <= set(row.keys())]
     after += len(merged[key])
 print(f"Merging, {before} before and {after} after removing things without all features")
 json.dump(merged, open(os.path.join(dataset_dir, 'metadata.json'), 'w'), indent=2)
